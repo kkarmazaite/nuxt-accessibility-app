8568a9433b5e45f8195bd33b5c46420510c15efe
Karolina Karmazaitė <karolina.karmazaite@cec.valantic.com>
2025-12-08 14:45:22 +0200

    Fix accessibility in the menu navigation

--- app/components/nav/Desktop.vue
+++ app/components/nav/Desktop.vue
@@ -1,24 +1,78 @@
 <script setup lang="ts">
     const { menuItems } = defineProps(['menuItems'])
+    const openMenu = ref<string | null>(null)
+
+    const toggleMenu = (id: string) => {
+        openMenu.value = openMenu.value === id ? null : id
+    }
+
+    onMounted(() => {
+        document.addEventListener('keydown', (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+            openMenu.value = null
+            }
+        })
+    })
 </script>
 
 <template>
-    <nav class="flex items-center">
-        <ul class="flex">
-            <li class="relative group" v-for="menuItem in menuItems" :key="menuItem.id">
-                <a class="block h-[56px] px-3 uppercase text-white flex items-center justify-center gap-2" :href="menuItem.link">
-                    <span>{{ menuItem.name }}</span>
-                    <Icon v-if="menuItem.children.length > 0" name="uil:angle-down" />
-                </a>
-                <ul class="absolute top-full left-0 bg-gray-600 hidden group-hover:block" v-if="menuItem.children.length > 0">
-                    <li v-for="subMenuItem in menuItem.children" :key="subMenuItem.id">
-                        <a class="block h-[81px] w-[240px] px-3 uppercase text-white flex items-center" :href="subMenuItem.link">{{ subMenuItem.name }}</a>
-                    </li>
-                </ul>
-            </li>
+  <nav class="flex items-center" aria-label="Main navigation">
+    <ul class="flex" role="menubar">
+      <li 
+        v-for="menuItem in menuItems" 
+        :key="menuItem.id" 
+        class="relative group"
+        role="none"
+      >
+        <button
+          v-if="menuItem.children.length"
+          class="block h-[56px] px-3 uppercase text-white flex items-center justify-center gap-2 focus:ring-2"
+          role="menuitem"
+          :aria-haspopup="true"
+          :aria-expanded="openMenu === menuItem.id"
+          @click="toggleMenu(menuItem.id)"
+        >
+          <span>{{ menuItem.name }}</span>
+          <Icon name="uil:angle-down" />
+        </button>
+
+        <a
+          v-else
+          class="block h-[56px] px-3 uppercase text-white flex items-center justify-center gap-2 focus:ring-2"
+          role="menuitem"
+          :href="menuItem.link"
+        >
+          {{ menuItem.name }}
+        </a>
+
+        <ul 
+          v-if="menuItem.children.length"
+          class="absolute top-full left-0 bg-gray-600"
+          :class="{ 'hidden': openMenu !== menuItem.id }"
+          role="menu"
+        >
+          <li 
+            v-for="subMenuItem in menuItem.children" 
+            :key="subMenuItem.id"
+            role="none"
+          >
+            <a 
+              class="block h-[81px] w-[240px] px-3 uppercase text-white flex items-center focus:ring-2"
+              role="menuitem"
+              :href="subMenuItem.link"
+            >
+              {{ subMenuItem.name }}
+            </a>
+          </li>
         </ul>
-        <div class="text-white pl-3">
-            <Icon name="uil:search" />
-        </div>
-    </nav>
+      </li>
+    </ul>
+
+    <button
+      class="text-white pl-3 focus:ring-2"
+      aria-label="Search"
+    >
+      <Icon name="uil:search" />
+    </button>
+  </nav>
 </template>
--- app/components/nav/Mobile.vue
+++ app/components/nav/Mobile.vue
@@ -1,30 +1,94 @@
 <script setup lang="ts">
     const { menuItems } = defineProps(['menuItems'])
     const showMenu = ref(false)
+    const openSubmenu = ref<string | null>(null)
 
     const toggleMenu = () => {
-        showMenu.value = !showMenu.value
+    showMenu.value = !showMenu.value
+    if (!showMenu.value) openSubmenu.value = null
     }
+
+    const toggleSubmenu = (id: string) => {
+    openSubmenu.value = openSubmenu.value === id ? null : id
+    }
+
+    const handleEscape = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+        showMenu.value = false
+        openSubmenu.value = null
+    }
+    }
+
+    onMounted(() => document.addEventListener('keydown', handleEscape))
+    onUnmounted(() => document.removeEventListener('keydown', handleEscape))
 </script>
 
 <template>
-    <nav class="relative">
-        <button class="w-10 h-10 text-white bg-gray-500 flex items-center justify-center cursor-pointer" @click="toggleMenu">
-            <Icon name="uil:bars" />
-        </button>
+  <nav aria-label="Mobile navigation" class="relative">
+    <button
+      class="w-10 h-10 text-white bg-gray-500 flex items-center justify-center cursor-pointer focus:ring-2"
+      @click="toggleMenu"
+      :aria-expanded="showMenu ? 'true' : 'false'"
+      aria-controls="mobile-nav"
+      aria-label="Open menu"
+    >
+      <Icon name="uil:bars" />
+    </button>
 
-        <ul class="absolute top-[calc(100%+15px)] right-0 bg-white w-[100vw] -mr-5" v-if="showMenu">
-            <li class="group" v-for="menuItem in menuItems" :key="menuItem.id">
-                <a class="block h-[56px] px-3 uppercase text-gray-500 flex items-center gap-2 border-b border-gray-2500" :href="menuItem.link">
-                    <span>{{ menuItem.name }}</span>
-                    <Icon v-if="menuItem.children.length > 0" name="uil:angle-down" />
-                </a>
-                <ul class="bg-gray-600 hidden group-hover:block" v-if="menuItem.children.length > 0">
-                    <li v-for="subMenuItem in menuItem.children" :key="subMenuItem.id">
-                        <a class="block h-[81px] w-[240px] px-3 uppercase text-white flex items-center" :href="subMenuItem.link">{{ subMenuItem.name }}</a>
-                    </li>
-                </ul>
+    <ul
+      v-if="showMenu"
+      id="mobile-nav"
+      role="menu"
+      class="absolute top-[calc(100%+15px)] right-0 bg-white w-[100vw] -mr-5"
+    >
+      <li
+        v-for="menuItem in menuItems"
+        :key="menuItem.id"
+        role="none"
+      >
+        <template v-if="menuItem.children.length">
+          <button
+            class="w-full flex justify-between items-center h-[56px] px-3 uppercase text-gray-700 border-b focus:ring-2"
+            role="menuitem"
+            @click="toggleSubmenu(menuItem.id)"
+            :aria-expanded="openSubmenu === menuItem.id"
+            aria-haspopup="true"
+          >
+            {{ menuItem.name }}
+            <Icon name="uil:angle-down" />
+          </button>
+
+          <ul
+            v-if="openSubmenu === menuItem.id"
+            role="menu"
+            class="bg-gray-600"
+          >
+            <li
+              v-for="subMenuItem in menuItem.children"
+              :key="subMenuItem.id"
+              role="none"
+            >
+              <a
+                class="block h-[56px] px-3 uppercase text-white flex items-center border-b focus:ring-2"
+                role="menuitem"
+                :href="subMenuItem.link"
+              >
+                {{ subMenuItem.name }}
+              </a>
             </li>
-        </ul>
-    </nav>
+          </ul>
+        </template>
+
+        <template v-else>
+          <a
+            class="block h-[56px] px-3 uppercase text-gray-700 flex items-center border-b focus:ring-2"
+            role="menuitem"
+            :href="menuItem.link"
+          >
+            {{ menuItem.name }}
+          </a>
+        </template>
+      </li>
+    </ul>
+  </nav>
 </template>
